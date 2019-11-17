6262de2b91a652a57af1e4bda8f6d231c592c6f4
CrisptyDP <crisdp30@gmail.com>
2019-11-17 17:46:19 -0500

    Supplier Controller

--- src/main/java/com/hampcode/ServletInitializer.java
+++ src/main/java/com/hampcode/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(RapifarmaApplication.class);
+		return application.sources(RapifarmaAppApplication.class);
 	}
 
 }
