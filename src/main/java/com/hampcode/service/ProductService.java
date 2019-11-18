ae2153e69ae30e87e315815f5c8d4832212a270b
unknown <sofya285@gmail.com>
2019-11-17 20:37:13 -0500

    ProductService

--- src/main/java/com/hampcode/service/ProductService.java
+++ src/main/java/com/hampcode/service/ProductService.java
@@ -1,5 +1,11 @@
 package com.hampcode.service;
 
-public interface ProductService {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import com.rapifarma.model.entity.Product;
+
+public interface ProductService extends CrudService<Product, Long>{
+	public Page<Product> findAll(Pageable pageable) throws Exception;
+	public Page<Product> fetchByName (String name, Pageable pageable) throws Exception;	
 }
